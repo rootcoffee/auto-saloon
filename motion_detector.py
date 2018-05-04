75d6f7d4ab4d99ac47f7b6d9cf0af0848041ed11
ROOT Cafe <socios@rootcoffeehouse.com.ar>
2018-05-04 19:18:56 -0300

    latest version, working one!

--- motion_detector.py
+++ motion_detector.py
@@ -1,12 +1,18 @@
 # Imports
 import time
+import datetime
+from subprocess import call
 from gpiozero import MotionSensor
+import subprocess
 
 #define the pins
 pir = MotionSensor(14)
 
+
 while True:
     if pir.motion_detected:
-        print("You moved")
+        now = datetime.datetime.now()
+        print (str(now) + " - Movimiento detectado en el salon.")
+        call(["/usr/local/bin/telegram-send", "Movimiento detectado en salon.")
     time.sleep(2)
 
